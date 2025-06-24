884ff378d1679d68b55937fe260b330cc3ed34a1
koguchi-e <koguchi.e915@gmail.com>
2025-06-24 10:09:18 +0900

    不要な.to_iを削除

--- cafe.rb
+++ cafe.rb
@@ -30,5 +30,5 @@ drink_order = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 food_order = take_order(FOODS)
 
-total = DRINKS[drink_order][:price].to_i + FOODS[food_order][:price].to_i
+total = DRINKS[drink_order][:price] + FOODS[food_order][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
